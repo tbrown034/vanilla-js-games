1d927a418371aed6f55ba3016149594e0f0a4385
tbrown034 <tbrown034@gmail.com>
2023-11-07 15:29:14 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("hello world");
+// Add this to your JavaScript file
+document
+  .getElementById("selectTicTacToe")
+  .addEventListener("click", function () {
+    // Code to start Tic Tac Toe game or navigate to the game's page
+    alert("Tic Tac Toe clicked!");
+  });
+
+document
+  .getElementById("selectConnectFour")
+  .addEventListener("click", function () {
+    // Code to start Connect Four game or navigate to the game's page
+    alert("Connect Four clicked!");
+  });
+
+document.getElementById("selectChess").addEventListener("click", function () {
+  // Code to start Chess game or navigate to the game's page
+  alert("Chess clicked!");
+});
